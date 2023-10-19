5709d2570f7f605892258028ad3fe60ef1b525ed
Muhammad Imaduddin <im@dudd.in>
2023-10-19 21:43:42 +0700

    replace tmux with zellij
    Less glitchy, more features

--- neovim/.config/nvim/lua/plugins/core.lua
+++ neovim/.config/nvim/lua/plugins/core.lua
@@ -192,14 +192,4 @@ return {
 		event = "BufReadPre",
 		opts = {},
 	},
-	{
-		"christoomey/vim-tmux-navigator",
-		keys = {
-			{ "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
-			{ "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
-			{ "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
-			{ "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
-		},
-		lazy = false,
-	},
 }
