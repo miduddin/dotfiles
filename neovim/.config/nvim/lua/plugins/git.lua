0783d6b2a58e8f878c2e603a8730ebe4d957f52b
Muhammad Imaduddin <im@dudd.in>
2023-10-21 22:05:34 +0700

    neovim: jump to git hunk with preview

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -60,8 +60,6 @@ return {
 					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
 				end
 
-				map("n", "]h", gs.next_hunk, "Next Hunk")
-				map("n", "[h", gs.prev_hunk, "Prev Hunk")
 				map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
 				map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
 				map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
@@ -71,8 +69,11 @@ return {
 				map("n", "<leader>ghd", gs.diffthis, "Diff This")
 				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 				-- stylua: ignore start
+				map("n", "]h", function() gs.next_hunk({preview = true}) end, "Next Hunk")
+				map("n", "[h", function() gs.prev_hunk({preview = true}) end, "Prev Hunk")
 				map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
 				map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+				-- stylua: ignore end
 			end,
 		},
 	},
