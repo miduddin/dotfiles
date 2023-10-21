fb3682d9df3db6622e735cc263f4a8f966173f7b
Muhammad Imaduddin <im@dudd.in>
2023-10-21 22:27:08 +0700

    fish: add keybind for clear buffer

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,14 @@
 function fish_user_key_bindings
+	function cl
+		clear
+		commandline -f repaint
+	end
+
+	bind \cl cl
+
+	if bind -M insert > /dev/null 2>&1
+		bind -M insert \cl cl
+	end
 end
 
 fzf_key_bindings
