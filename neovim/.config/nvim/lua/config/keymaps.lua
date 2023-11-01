bfc72ccddaba438429be601dd70b305ec7f497de
Muhammad Imaduddin <im@dudd.in>
2023-11-01 21:44:06 +0700

    neovim: cleanup keymaps.lua

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,11 +1,6 @@
 local function map(mode, lhs, rhs, opts)
 	local keys = require("lazy.core.handler").handlers.keys
 	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-		opts = opts or {}
-		opts.silent = opts.silent ~= false
-		if opts.remap and not vim.g.vscode then
-			opts.remap = nil
-		end
 		vim.keymap.set(mode, lhs, rhs, opts)
 	end
 end
@@ -16,10 +11,11 @@ map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line up" })
 map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line down" })
 map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move lines up" })
 map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move lines down" })
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", noremap = true })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to below window", noremap = true })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to above window", noremap = true })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", noremap = true })
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to below window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to above window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+map("n", "*", "*``")
 map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close tab" })
 map("n", "<leader>w", "<cmd>set wrap!<cr>", { desc = "Toggle word wrap" })
 map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to clipboard" })
@@ -48,12 +44,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- map("n", "<leader>D", vim.lsp.buf.type_definition, { desc = "Type definition", buffer = ev.buf })
 		map("n", "K", vim.lsp.buf.hover, { desc = "Hover", buffer = ev.buf })
 		map("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename", buffer = ev.buf })
+		map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename", buffer = ev.buf })
 		map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action", buffer = ev.buf })
-		map(
-			"n",
-			"<leader>ci",
-			"<cmd>lua vim.lsp.inlay_hint(0, nil)<cr>",
-			{ desc = "Toggle inlay hint", buffer = ev.buf }
-		)
+		map("n", "<leader>ci", "<cmd>lua vim.lsp.inlay_hint(0)<cr>", { desc = "Toggle inlay hint", buffer = ev.buf })
 	end,
 })
