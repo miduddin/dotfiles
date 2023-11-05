cac1d16269ead904426685c25478b25f7d3f1bff
Muhammad Imaduddin <im@dudd.in>
2023-11-05 22:16:40 +0700

    neovim: cleanup git config

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -1,6 +1,3 @@
--- git-blame.nvim config
-vim.g.gitblame_enabled = 0
-
 return {
 	{
 		"sindrets/diffview.nvim",
@@ -14,12 +11,20 @@ return {
 						vim.cmd("DiffviewOpen " .. ref)
 					end
 				end,
-				desc = "Diff with ref",
+				desc = "Diff with ref...",
 			},
+			{ "<leader>gl", "<Cmd>DiffviewFileHistory %<cr>", desc = "Current file history" },
+			{ "<leader>gl", ":DiffviewFileHistory<cr>", desc = "Selected lines history", mode = "v" },
+			{ "<leader>gL", "<Cmd>DiffviewFileHistory<cr>", desc = "Project history" },
 		},
 		opts = {
 			default_args = {
 				DiffviewOpen = { "--imply-local" },
+			},
+			keymaps = {
+				file_history_panel = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
+				file_panel = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
+				view = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
 			},
 		},
 	},
@@ -33,47 +38,58 @@ return {
 			{ "<leader>goc", "<cmd>GitBlameOpenCommitURL<cr>", desc = "Open commit in browser", mode = "n" },
 			{ "<leader>gof", "<cmd>GitBlameOpenFileURL<cr>", desc = "Open file in browser", mode = "n" },
 		},
+		opts = {
+			enabled = false,
+		},
 	},
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = {
-			current_line_blame_opts = {
-				delay = 0,
-			},
+			current_line_blame_opts = { delay = 0 },
 			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <abbrev_sha> <summary>",
-			preview_config = {
-				border = "rounded",
-			},
-			signs = {
-				add = { text = "▎" },
-				change = { text = "▎" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▎" },
-				untracked = { text = "▎" },
-			},
-			on_attach = function(buffer)
+			preview_config = { border = "rounded" },
+			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
 
-				local function map(mode, l, r, desc)
-					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
 				end
 
-				map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-				map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-				map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-				map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-				map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-				map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-				map("n", "<leader>ghd", gs.diffthis, "Diff This")
-				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(gs.next_hunk)
+					return "<Ignore>"
+				end, { expr = true })
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(gs.prev_hunk)
+					return "<Ignore>"
+				end, { expr = true })
+
+				map("n", "<leader>hs", gs.stage_hunk)
+				map("n", "<leader>hr", gs.reset_hunk)
+				map("n", "<leader>hS", gs.stage_buffer)
+				map("n", "<leader>hu", gs.undo_stage_hunk)
+				map("n", "<leader>hR", gs.reset_buffer)
+				map("n", "<leader>hp", gs.preview_hunk)
+				map("n", "<leader>hd", gs.toggle_deleted)
 				-- stylua: ignore start
-				map("n", "]h", function() gs.next_hunk({preview = true}) end, "Next Hunk")
-				map("n", "[h", function() gs.prev_hunk({preview = true}) end, "Prev Hunk")
-				map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
-				map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+				map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+				map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
+				map("n", "<leader>hD", function() gs.diffthis("~") end)
+				map("n", "<leader>hb", function() gs.blame_line({ full = true }) end)
 				-- stylua: ignore end
+
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
 			end,
 		},
 	},
