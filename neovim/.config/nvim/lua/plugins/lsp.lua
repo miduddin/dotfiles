033be2d07a310fb2407957cfe6d1eb4a8a1a63c9
Muhammad Imaduddin <im@dudd.in>
2023-11-06 23:01:00 +0700

    neovim: disable <tab> to expand snippet

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -117,15 +117,15 @@ return {
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
+						if luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
 						else
 							fallback()
 						end
 					end, { "i", "s" }),
 
 					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.jumpable(-1) then
+						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
 						else
 							fallback()
