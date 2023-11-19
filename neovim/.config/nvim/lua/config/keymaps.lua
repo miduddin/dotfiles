c4a54defdbd4a165878a9fc36e4f384d87ba016b
Muhammad Imaduddin <im@dudd.in>
2023-11-19 21:29:19 +0700

    neovim: add select all keymap
    Also delete unused keymap.

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,22 +1,28 @@
 local map = vim.keymap.set
 
 map({ "n", "t" }, "<esc>", "<cmd>noh<cr><esc>")
+
+map("n", "<C-a>", "ggVG", { desc = "Select all" })
 map("n", "<C-s>", "<cmd>w<cr>", { desc = "Save file" })
+
 map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line up" })
 map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line down" })
 map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move lines up" })
 map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move lines down" })
+
 map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to below window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to above window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
 map("n", "*", "*``")
 map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close tab" })
 map("n", "<leader>w", "<cmd>set wrap!<cr>", { desc = "Toggle word wrap" })
+
 map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to clipboard" })
 map({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
 map({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste from clipboard" })
-map({ "i", "t" }, "<M-BS>", "<C-w>", { desc = "Delete word" })
+
 map("n", "<S-h>", "<cmd>bp<cr>", { desc = "Prev buffer" })
 map("n", "<S-l>", "<cmd>bn<cr>", { desc = "Next buffer" })
 map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Close current buffer" })
