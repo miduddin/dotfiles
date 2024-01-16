369cefa2e8299aa7dd388d51a399259be2d115d7
Muhammad Imaduddin <im@dudd.in>
2024-01-16 22:06:59 +0700

    neovim: add tailwindcss lsp

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -38,6 +38,7 @@ return {
 			lspconfig.lua_ls.setup(default_setup)
 			lspconfig.intelephense.setup(default_setup)
 			lspconfig.rust_analyzer.setup(default_setup)
+			lspconfig.tailwindcss.setup(default_setup)
 			lspconfig.gopls.setup(default_with({
 				settings = {
 					gopls = {
