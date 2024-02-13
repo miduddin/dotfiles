10df1e1e388b4543e25a4978bd5dff6c9f6579a6
Muhammad Imaduddin <im@dudd.in>
2024-02-13 23:46:36 +0700

    neovim: change 'delete other buffers' keymap to 'delete all buffers'

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -109,7 +109,7 @@ return {
 		event = "VeryLazy",
 		keys = {
 			{ "<leader>bd", "<cmd>BufDel<cr>", { desc = "Close current buffer" } },
-			{ "<leader>bD", "<cmd>BufDelOthers<cr>", { desc = "Close all other buffers" } },
+			{ "<leader>bD", "<cmd>BufDelAll<cr>", { desc = "Close all other buffers" } },
 		},
 		opts = { quit = false },
 	},
