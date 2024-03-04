e4dcb760243cce8e54668b6bfb534725ed1a13e7
Muhammad Imaduddin <im@dudd.in>
2024-03-04 22:21:51 +0700

    neovim: tidy up test & debug config

--- neovim/.config/nvim/lua/plugins/test.lua
+++ neovim/.config/nvim/lua/plugins/test.lua
@@ -6,6 +6,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-neotest/neotest-go",
 			"olimorris/neotest-phpunit",
+			"rcarriga/nvim-dap-ui",
 		},
 		keys = {
 			-- stylua: ignore start
@@ -57,112 +58,6 @@ return {
 	},
 	{
 		"mfussenegger/nvim-dap",
-		dependencies = {
-			{
-				"rcarriga/nvim-dap-ui",
-				keys = {
-					-- stylua: ignore start
-					{ "<leader>du", function() require("dapui").toggle({ reset = true }) end, desc = "Dap UI" },
-					{ "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
-					-- stylua: ignore end
-				},
-				opts = {
-					icons = { expanded = "🞃", collapsed = "🞂", current_frame = "→" },
-					controls = {
-						icons = {
-							pause = "⏸",
-							play = "⯈",
-							step_into = "↴",
-							step_over = "↷",
-							step_out = "↑",
-							step_back = "↶",
-							run_last = "🗘",
-							terminate = "🕱",
-							disconnect = "⏻",
-						},
-					},
-					layouts = {
-						{
-							elements = {
-								{ id = "scopes", size = 0.25 },
-								{ id = "breakpoints", size = 0.25 },
-								{ id = "watches", size = 0.25 },
-								{ id = "stacks", size = 0.25 },
-							},
-							position = "left",
-							size = 40,
-						},
-						{
-							elements = {
-								{ id = "repl", size = 1 },
-							},
-							position = "bottom",
-							size = 10,
-						},
-					},
-				},
-				config = function(_, opts)
-					local dap = require("dap")
-					local dapui = require("dapui")
-					dapui.setup(opts)
-					dap.listeners.after.event_initialized["dapui_config"] = function()
-						dapui.open({})
-					end
-					dap.listeners.before.event_terminated["dapui_config"] = function()
-						dapui.close({})
-					end
-					dap.listeners.before.event_exited["dapui_config"] = function()
-						dapui.close({})
-					end
-				end,
-			},
-			{
-				"leoluz/nvim-dap-go",
-				dependencies = { "williamboman/mason.nvim" },
-				ft = { "go" },
-				config = function()
-					require("dap-go").setup({})
-
-					vim.api.nvim_create_autocmd("FileType", {
-						pattern = { "go" },
-						callback = function(ev)
-							vim.keymap.set(
-								"n",
-								"<leader>td",
-								"<cmd>lua require('dap-go').debug_test()<CR>",
-								{ desc = "Debug nearest (go)", buffer = ev.buf }
-							)
-						end,
-					})
-				end,
-			},
-		},
-		keys = {
-			-- stylua: ignore start
-			{ "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition" },
-			{ "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-			{ "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
-			{ "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
-			{ "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
-			{ "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
-			{ "<leader>dj", function() require("dap").down() end, desc = "Down" },
-			{ "<leader>dk", function() require("dap").up() end, desc = "Up" },
-			{ "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
-			{ "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
-			{ "<leader>dn", function() require("dap").step_over() end, desc = "Step Over" },
-			{ "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
-			{ "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
-			{ "<leader>ds", function() require("dap").session() end, desc = "Session" },
-			{ "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
-			{ "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Inspect Value", mode = {"n", "v"} },
-
-			-- Alternative keymaps:
-			{ "<F5>", function() require("dap").continue() end, desc = "Continue" },
-			{ "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
-			{ "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
-			{ "<F12>", function() require("dap").step_out() end, desc = "Step Out" },
-			-- stylua: ignore end
-		},
 		config = function()
 			local dap = require("dap")
 			dap.adapters.php = {
@@ -175,16 +70,92 @@ return {
 					request = "launch",
 					name = "Listen for Xdebug",
 					port = 9003,
-					-- xdebugSettings = {
-					-- 	max_children = 999,
-					-- 	max_depth = 10,
-					-- 	max_data = 10240,
-					-- },
-					-- pathMappings = {
-					-- 	["/app"] = "${workspaceFolder}",
-					-- },
 				},
 			}
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		keys = {
+			-- stylua: ignore start
+			{ "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition" },
+			{ "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+			{ "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+			{ "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+			{ "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
+			{ "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+			{ "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+			{ "<leader>dj", function() require("dap").down() end, desc = "Down" },
+			{ "<leader>dk", function() require("dap").up() end, desc = "Up" },
+			{ "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+			{ "<leader>dn", function() require("dap").step_over() end, desc = "Step Over" },
+			{ "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+			{ "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+			{ "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+			{ "<leader>ds", function() require("dap").session() end, desc = "Session" },
+			{ "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+			{ "<leader>du", function() require("dapui").toggle({ reset = true }) end, desc = "Dap UI" },
+			{ "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Inspect Value", mode = {"n", "v"} },
+
+			-- Alternative keymaps:
+			{ "<F5>", function() require("dap").continue() end, desc = "Continue" },
+			{ "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
+			{ "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
+			{ "<F12>", function() require("dap").step_out() end, desc = "Step Out" },
+			-- stylua: ignore end
+		},
+		opts = {
+			icons = { expanded = "🞃", collapsed = "🞂", current_frame = "→" },
+			controls = {
+				icons = {
+					pause = "⏸",
+					play = "⯈",
+					step_into = "↴",
+					step_over = "↷",
+					step_out = "↑",
+					step_back = "↶",
+					run_last = "🗘",
+					terminate = "🕱",
+					disconnect = "⏻",
+				},
+			},
+			layouts = {
+				{
+					elements = {
+						{ id = "scopes", size = 0.25 },
+						{ id = "breakpoints", size = 0.25 },
+						{ id = "watches", size = 0.25 },
+						{ id = "stacks", size = 0.25 },
+					},
+					position = "left",
+					size = 40,
+				},
+				{
+					elements = {
+						{ id = "repl", size = 1 },
+					},
+					position = "bottom",
+					size = 10,
+				},
+			},
+		},
+		config = function(_, opts)
+			local dap, dapui = require("dap"), require("dapui")
+			dapui.setup(opts)
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
 
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = { "dap-float" },
@@ -195,6 +166,29 @@ return {
 						buffer = ev.buf,
 						command = "q",
 					})
+				end,
+			})
+		end,
+	},
+	{
+		"leoluz/nvim-dap-go",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+		},
+		ft = { "go" },
+		config = function()
+			require("dap-go").setup()
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "go" },
+				callback = function(ev)
+					vim.keymap.set(
+						"n",
+						"<leader>td",
+						"<cmd>lua require('dap-go').debug_test()<CR>",
+						{ desc = "Debug nearest (go)", buffer = ev.buf }
+					)
 				end,
 			})
 		end,
