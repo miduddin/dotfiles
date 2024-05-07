e3e9440d3458571c389fdd3cef21945fac85b80d
Muhammad Imaduddin <im@dudd.in>
2024-05-07 16:59:56 +0700

    fish: add fzf header

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -11,15 +11,18 @@ function fish_user_key_bindings
 end
 
 function __fzf_history -d "Show command history"
-	set -lx FZF_DEFAULT_OPTS "--height 10 --scheme=history $FZF_DEFAULT_OPTS"
-	history -z | eval "fzf" --read0 --print0 -q '(commandline)' | read -lz result
+	history -z |
+		fzf --height 10 --scheme=history --header "Command history:" --read0 --print0 |
+		read -lz result
 	and commandline -- $result
 	commandline -f repaint
 end
 
 function __fzf_fd_nvim -d "Change directory and open neovim"
-	set -lx FZF_DEFAULT_OPTS "--height 10 --scheme=path $FZF_DEFAULT_OPTS"
-	fd -d 4 -t d -I --prune --hidden '\.git$' "$HOME" -x echo {//} | sort | fzf | read -l result
+	fd -d 4 -t d -I --prune --hidden '\.git$' "$HOME" -x echo {//} |
+		sort |
+		fzf --height 10 --scheme=path --header 'Git projects:' |
+		read -l result
 	and cd $result
 	and commandline -- nvim
 	commandline -f repaint
