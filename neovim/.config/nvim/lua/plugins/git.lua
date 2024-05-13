0ef803469cf43de516990ed3a6dfa4af8415d256
Muhammad Imaduddin <im@dudd.in>
2024-05-13 18:00:59 +0700

    neovim: swap gitsigns with mini.diff
    Less unused functionality + simpler setup.

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -43,54 +43,20 @@ return {
 		},
 	},
 	{
-		"lewis6991/gitsigns.nvim",
+		"echasnovski/mini.diff",
 		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			current_line_blame_opts = { delay = 0 },
-			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <abbrev_sha> <summary>",
-			preview_config = { border = "rounded" },
-			on_attach = function(bufnr)
-				local gs = package.loaded.gitsigns
+		config = function()
+			require("mini.diff").setup({
+				view = {
+					priority = 1,
+					style = "sign",
+					signs = { add = "┃", change = "┃", delete = "_" },
+				},
+			})
 
-				local function map(mode, l, r, opts)
-					opts = opts or {}
-					opts.buffer = bufnr
-					vim.keymap.set(mode, l, r, opts)
-				end
-
-				-- Navigation
-				map("n", "]c", function()
-					if vim.wo.diff then
-						return "]c"
-					end
-					vim.schedule(gs.next_hunk)
-					return "<Ignore>"
-				end, { expr = true })
-
-				map("n", "[c", function()
-					if vim.wo.diff then
-						return "[c"
-					end
-					vim.schedule(gs.prev_hunk)
-					return "<Ignore>"
-				end, { expr = true })
-
-				map("n", "<leader>hs", gs.stage_hunk)
-				map("n", "<leader>hr", gs.reset_hunk)
-				map("n", "<leader>hS", gs.stage_buffer)
-				map("n", "<leader>hu", gs.undo_stage_hunk)
-				map("n", "<leader>hR", gs.reset_buffer)
-				map("n", "<leader>hp", gs.preview_hunk)
-				map("n", "<leader>hd", gs.toggle_deleted)
-				-- stylua: ignore start
-				map("v", "<leader>hs", function() gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
-				map("v", "<leader>hr", function() gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end)
-				map("n", "<leader>hD", function() gs.diffthis("~") end)
-				map("n", "<leader>hb", function() gs.blame_line({ full = true }) end)
-				-- stylua: ignore end
-
-				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-			end,
-		},
+			vim.api.nvim_set_hl(0, "MiniDiffSignAdd", { link = "diffAdded" })
+			vim.api.nvim_set_hl(0, "MiniDiffSignChange", { link = "diffChanged" })
+			vim.api.nvim_set_hl(0, "MiniDiffSignDelete", { link = "diffDeleted" })
+		end,
 	},
 }
