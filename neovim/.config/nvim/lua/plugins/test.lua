b348ee389383862907c9b7ed48cc692072d17fc7
Muhammad Imaduddin <im@dudd.in>
2024-06-10 20:20:12 +0700

    neovim: remove dap/dapui listener
    Seems to cause hangs on exit after opening dapui together with neotest.

--- neovim/.config/nvim/lua/plugins/test.lua
+++ neovim/.config/nvim/lua/plugins/test.lua
@@ -164,21 +164,8 @@ return {
 			},
 		},
 		config = function(_, opts)
-			local dap, dapui = require("dap"), require("dapui")
+			local dapui = require("dapui")
 			dapui.setup(opts)
-
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
 
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = { "dap-float" },
