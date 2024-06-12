bdc42d2a3d671d9d95eae8f28642f41bce0719c3
Muhammad Imaduddin <im@dudd.in>
2024-06-12 19:54:21 +0700

    fish: ensure binaries for fish helpers exist

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,14 @@ if status is-interactive
 	# Commands to run in interactive sessions can go here
 end
 
-source ~/.phpbrew/phpbrew.fish
-direnv hook fish | source
+if test -f ~/.phpbrew/phpbrew.fish
+	source ~/.phpbrew/phpbrew.fish
+end
 
-starship init fish | source
+if type -q direnv
+	direnv hook fish | source
+end
+
+if type -q starship
+	starship init fish | source
+end
