fe3d87faca119ad659849a2887fa90022de68308
Muhammad Imaduddin <im@dudd.in>
2024-06-19 16:33:10 +0700

    fish: k can be k9s or kubectl depending on argc

--- fish/.config/fish/functions/k.fish
+++ fish/.config/fish/functions/k.fish
@@ -1,3 +1,7 @@
-function k -w k9s
-	k9s --context $argv[1] $argv[2..]
+function k
+	if test (count $argv) -eq 1
+		k9s --context $argv[1]
+	else
+		kubectl --context $argv[1] -n $argv[2] $argv[3..]
+	end
 end
