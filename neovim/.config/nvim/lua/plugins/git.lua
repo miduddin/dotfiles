85b4e5fd368a0e14822d854d91497409512bf4c0
Muhammad Imaduddin <im@dudd.in>
2024-06-25 19:05:04 +0700

    neovim: use treesitter folding
    Except when in diffview.

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -26,6 +26,11 @@ return {
 				file_panel = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
 				view = { { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close Diffview" } } },
 			},
+			hooks = {
+				diff_buf_read = function(bufnr)
+					vim.opt_local.foldtext = "foldtext()"
+				end,
+			},
 		},
 	},
 	{
