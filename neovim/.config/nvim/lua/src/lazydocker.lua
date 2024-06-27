bb878a0e8e5d449c6c076d8c80f00809e259e60f
Muhammad Imaduddin <im@dudd.in>
2024-06-27 22:47:07 +0700

    neovim: remove dependency to zellij

---
+++ neovim/.config/nvim/lua/src/lazydocker.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	pattern = "term://*lazydocker",
+	command = "startinsert | setlocal nonumber signcolumn=no",
+})
+
+vim.api.nvim_create_autocmd({ "TermClose" }, {
+	pattern = "term://*lazydocker",
+	command = [[call feedkeys("k")]],
+})
+
+vim.keymap.set("n", "<leader>/d", "<Cmd>tabnew | term lazydocker<CR>", { desc = "Lazydocker" })
