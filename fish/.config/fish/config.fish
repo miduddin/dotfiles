479ce874852d583505d85e0148edc65ece168823
Muhammad Imaduddin <im@dudd.in>
2024-06-27 23:33:27 +0700

    windows terminal: native pane keymaps

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
 	# Commands to run in interactive sessions can go here
 end
 
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+      printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
+
 if test -f ~/.phpbrew/phpbrew.fish
 	source ~/.phpbrew/phpbrew.fish
 end
