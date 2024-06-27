fc452feb060bda094597078b73b62df219015ce5
Muhammad Imaduddin <im@dudd.in>
2024-06-27 22:47:07 +0700

    neovim: remove dependency to zellij

---
+++ neovim/.config/nvim/lua/src/lazygit.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+	pattern = "term://*lazygit",
+	command = "startinsert | setlocal nonumber signcolumn=no",
+})
+
+vim.api.nvim_create_autocmd({ "TermClose" }, {
+	pattern = "term://*lazygit",
+	command = [[call feedkeys("k")]],
+})
+
+vim.keymap.set("n", "<leader>/g", "<Cmd>tabnew | term lazygit<CR>", { desc = "Lazygit" })
