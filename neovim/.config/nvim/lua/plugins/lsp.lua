49c21494e7a34292f58e9bf50b708cfbd4a9bf6f
Muhammad Imaduddin <im@dudd.in>
2024-07-02 02:39:51 +0700

    neovim: neovim lua lsp

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,16 @@ vim.lsp.set_log_level(vim.g.log_level)
 
 return {
 	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true },
+	{
 		"williamboman/mason.nvim",
 		cmd = { "Mason" },
 		keys = {
