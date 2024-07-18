6c103f0f721b6c599b628b48ed6d6bc935864faf
Muhammad Imaduddin <im@dudd.in>
2024-07-18 21:34:59 +0700

    neovim: add copy file path to clipboard command

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,9 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 vim.api.nvim_create_user_command("Q", "q", {})
 vim.api.nvim_create_user_command("Qa", "qa", {})
 vim.api.nvim_create_user_command("W", "w", {})
+
+vim.api.nvim_create_user_command("CopyRelPath", function()
+	local path = vim.fn.expand("%")
+	vim.fn.setreg("+", path)
+	vim.notify("Copied: " .. path)
+end, {})
