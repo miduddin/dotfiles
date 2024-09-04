063ea74a3094b0b9f6240e3acb3c5732af1a6880
Muhammad Imaduddin <im@dudd.in>
2024-09-04 16:05:08 +0700

    neovim: add connection name to sql query result title
    Also fix result buffer name in statusline.

--- neovim/.config/nvim/lua/src/utils.lua
+++ neovim/.config/nvim/lua/src/utils.lua
@@ -6,12 +6,12 @@ function M.write_cmd_output_to_split(obj, bufname)
 	vim.cmd("split")
 	local win = vim.api.nvim_get_current_win()
 	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_buf_set_name(buf, bufname)
 	vim.api.nvim_win_set_buf(win, buf)
 	vim.keymap.set("n", "q", "<Cmd>bd<CR>", { desc = "Close buffer", buffer = buf })
 
 	vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "Running..." })
 	vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
-	vim.api.nvim_buf_set_name(buf, bufname)
 	vim.cmd("redraw")
 
 	local result = obj:wait()
