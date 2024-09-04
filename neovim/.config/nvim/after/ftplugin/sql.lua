5c61f876d0600b93233a5227e662adcd01e778f0
Muhammad Imaduddin <im@dudd.in>
2024-09-04 20:24:19 +0700

    neovim: run last sql query with @: instead

--- neovim/.config/nvim/after/ftplugin/sql.lua
+++ neovim/.config/nvim/after/ftplugin/sql.lua
@@ -7,5 +7,3 @@ local usql = require("src.usql")
 vim.api.nvim_buf_create_user_command(0, "Usql", function(opts)
 	usql.query_paragraph(opts.fargs[1], opts.fargs[2])
 end, { nargs = "*" })
-
-vim.keymap.set("n", "<Leader>ql", usql.query_last, { desc = "Run query with last params", buffer = 0 })
