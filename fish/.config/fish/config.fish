9c1286f6e5deb5db9a34dfd487543e9209fc87b0
Muhammad Imaduddin <im@dudd.in>
2024-10-19 21:21:30 +0700

    macos adjustments

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,11 @@
 if status is-interactive
-	# Commands to run in interactive sessions can go here
+	set fish_vi_force_cursor 1
 end
 
 function storePathForWindowsTerminal --on-variable PWD
-    if test -n "$WT_SESSION"
-      printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
-    end
+	if test -n "$WT_SESSION"
+	  printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+	end
 end
 
 if test -f ~/.phpbrew/phpbrew.fish
