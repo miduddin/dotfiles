1c2f82da2cd826965df7c4281da93f1492be5264
Muhammad Imaduddin <im@dudd.in>
2024-11-20 10:37:58 +0700

    neovim: set cmd output buffer as scratch buffer

--- neovim/.config/nvim/lua/src/utils.lua
+++ neovim/.config/nvim/lua/src/utils.lua
@@ -3,12 +3,15 @@ local M = {}
 ---@param obj vim.SystemObj
 ---@param bufname string
 function M.write_cmd_output_to_split(obj, bufname)
-	vim.cmd("split")
-	local win = vim.api.nvim_get_current_win()
 	local buf = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_buf_set_name(buf, bufname)
-	vim.api.nvim_win_set_buf(win, buf)
+	vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf })
+	vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf })
 	vim.keymap.set("n", "q", "<Cmd>bd<CR>", { desc = "Close buffer", buffer = buf })
+
+	vim.cmd("split")
+	local win = vim.api.nvim_get_current_win()
+	vim.api.nvim_win_set_buf(win, buf)
 
 	vim.api.nvim_buf_set_lines(buf, 0, -1, true, { "Running..." })
 	vim.cmd("redraw")
