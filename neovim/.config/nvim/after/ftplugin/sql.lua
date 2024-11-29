377f078af84958a3fe9b29c03a51c2bb7ebf9556
Muhammad Imaduddin <im@dudd.in>
2024-11-29 16:04:10 +0700

    neovim: sql commentstring

--- neovim/.config/nvim/after/ftplugin/sql.lua
+++ neovim/.config/nvim/after/ftplugin/sql.lua
@@ -1,3 +1,5 @@
+vim.opt_local.commentstring = "-- %s"
+
 local psql = require("src.psql")
 vim.api.nvim_buf_create_user_command(0, "Psql", function(opts)
 	psql.query_paragraph(opts.fargs[1], opts.fargs[2])
