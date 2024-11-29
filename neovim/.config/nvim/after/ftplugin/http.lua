d8631635c9fe9bd3ac75dc042f370baba4b5055f
Muhammad Imaduddin <im@dudd.in>
2024-11-29 16:03:23 +0700

    neovim: re-add http request plugin
    Need it mainly for variable support.

--- neovim/.config/nvim/after/ftplugin/http.lua
+++ neovim/.config/nvim/after/ftplugin/http.lua
@@ -1,2 +1,2 @@
 vim.opt_local.commentstring = "# %s"
-vim.keymap.set("n", "<Leader>r", require("src.http").run, { desc = "Run nearest HTTP request", buffer = 0 })
+vim.keymap.set("n", "<Leader>r", require("kulala").run, { desc = "Run nearest HTTP request", buffer = 0 })
