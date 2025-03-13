7f4d35da27b45dab48ffe38ed4522d184b4b20f7
Muhammad Imaduddin <im@dudd.in>
2025-03-13 17:14:41 +0700

    neovim: swap multicursor plugin

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -5,13 +5,35 @@ return {
 		opts = {},
 	},
 	{
-		"mg979/vim-visual-multi",
-		event = { "BufReadPost", "BufNewFile" },
-		init = function()
-			vim.g.VM_maps = {
-				["Find Under"] = "<C-X>",
-				["Find Subword Under"] = "<C-X>",
-			}
+		"jake-stewart/multicursor.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local mc = require("multicursor-nvim")
+			mc.setup()
+
+			local set = vim.keymap.set
+
+			set({ "n", "x" }, "<Up>", function() mc.lineAddCursor(-1) end)
+			set({ "n", "x" }, "<Down>", function() mc.lineAddCursor(1) end)
+			set({ "n", "x" }, "<C-x>", function() mc.matchAddCursor(1) end)
+
+			mc.addKeymapLayer(function(layerSet)
+				layerSet({ "n", "x" }, "<Left>", mc.prevCursor)
+				layerSet({ "n", "x" }, "<Right>", mc.nextCursor)
+				layerSet({ "n", "x" }, "n", function() mc.matchAddCursor(1) end)
+				layerSet({ "n", "x" }, "q", function() mc.matchSkipCursor(0) end)
+				layerSet({ "n", "x" }, "N", function() mc.matchAddCursor(-1) end)
+				layerSet({ "n", "x" }, "Q", function() mc.matchSkipCursor(-1) end)
+				layerSet({ "n", "x" }, "<Leader>a", mc.alignCursors)
+				layerSet({ "n", "x" }, "<Leader>x", mc.deleteCursor)
+				layerSet("n", "<Esc>", function()
+					if not mc.cursorsEnabled() then
+						mc.enableCursors()
+					else
+						mc.clearCursors()
+					end
+				end)
+			end)
 		end,
 	},
 	{
