35e262550652d23f828103f7b03defa4395b850d
Muhammad Imaduddin <im@dudd.in>
2025-03-13 17:43:30 +0700

    neovim: simpler completion setup with blink.cmp

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -19,8 +19,9 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
+		version = "*",
 		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
+			"saghen/blink.cmp",
 			"williamboman/mason.nvim",
 		},
 		opts = {
@@ -87,7 +88,7 @@ return {
 			require("lspconfig.ui.windows").default_options.border = vim.g.border
 
 			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			local handlers = {
 				["textDocument/hover"] = vim.lsp.with(
 					vim.lsp.handlers.hover,
@@ -143,108 +144,29 @@ return {
 		},
 	},
 	{
-		"hrsh7th/nvim-cmp",
-		event = { "InsertEnter" },
-		dependencies = {
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-			"hrsh7th/cmp-buffer",
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
+		version = "*",
+		event = "InsertEnter",
+		opts = {
+			keymap = {
+				preset = "enter",
+				["<C-k>"] = { "select_prev", "fallback" },
+				["<C-j>"] = { "select_next", "fallback" },
+			},
+			completion = {
+				menu = { border = vim.g.border },
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 500,
+					window = { border = vim.g.border },
+				},
+			},
+			signature = {
+				enabled = true,
+				window = { border = vim.g.border },
+			},
 		},
-		config = function()
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			luasnip.log.set_loglevel("error")
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			local kinds = {
-				Text = "󰉿 ",
-				Method = "󰊕 ",
-				Function = "󰊕 ",
-				Constructor = "󰒓 ",
-				Field = "󰜢 ",
-				Variable = "󰆦 ",
-				Property = "󰖷 ",
-				Class = "󱡠 ",
-				Interface = "󱡠 ",
-				Struct = "󱡠 ",
-				Module = "󰅩 ",
-				Unit = "󰪚 ",
-				Value = "󰦨 ",
-				Enum = "󰦨 ",
-				EnumMember = "󰦨 ",
-				Keyword = "󰻾 ",
-				Constant = "󰏿 ",
-				Snippet = "󱄽 ",
-				Color = "󰏘 ",
-				File = "󰈔 ",
-				Reference = "󰬲 ",
-				Folder = "󰉋 ",
-				Event = "󱐋 ",
-				Operator = "󰪚 ",
-				TypeParameter = "󰬛 ",
-			}
-
-			cmp.setup({
-				snippet = {
-					expand = function(args) require("luasnip").lsp_expand(args.body) end,
-				},
-				window = {
-					completion = cmp.config.window.bordered({
-						border = vim.g.border,
-						winhighlight = "FloatBorder:FloatBorder",
-					}),
-					documentation = cmp.config.window.bordered({
-						border = vim.g.border,
-						winhighlight = "FloatBorder:FloatBorder",
-						max_width = 80,
-					}),
-				},
-				formatting = {
-					fields = { "kind", "abbr" },
-					format = function(_, vim_item)
-						if vim_item.menu and #vim_item.menu > 20 then
-							vim_item.menu = string.sub(vim_item.menu, 1, 20) .. "…"
-						end
-						vim_item.kind = kinds[vim_item.kind] or ""
-						return vim_item
-					end,
-				},
-				matching = {
-					disallow_partial_fuzzy_matching = false,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-D>"] = cmp.mapping.scroll_docs(-4),
-					["<C-F>"] = cmp.mapping.scroll_docs(4),
-					["<C-J>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-K>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-E>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.locally_jumpable(1) then
-							luasnip.jump(1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
 	},
 	{
 		"stevearc/conform.nvim",
