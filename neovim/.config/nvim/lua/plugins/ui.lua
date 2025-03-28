6ce63f409c826ad5501225111d03e38995cd9b72
Muhammad Imaduddin <im@dudd.in>
2025-03-28 20:32:45 +0700

    neovim: consolidate plugin highlights

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -15,28 +15,11 @@ return {
 			},
 			view_options = { show_hidden = true },
 		},
-		config = function(_, opts)
-			require("oil").setup(opts)
-			vim.api.nvim_set_hl(0, "OilDirHidden", { link = "OilDir" })
-			vim.api.nvim_set_hl(0, "OilSocketHidden", { link = "OilSocket" })
-			vim.api.nvim_set_hl(0, "OilLinkHidden", { link = "OilLink" })
-			vim.api.nvim_set_hl(0, "OilOrphanLinkHidden", { link = "OilOrphanLink" })
-			vim.api.nvim_set_hl(0, "OilLinkTargetHidden", { link = "OilLinkTarget" })
-			vim.api.nvim_set_hl(0, "OilOrphanLinkTargetHidden", { link = "OilOrphanLinkTarget" })
-			vim.api.nvim_set_hl(0, "OilFileHidden", { link = "OilFile" })
-		end,
 	},
 	{
 		"echasnovski/mini.tabline",
 		lazy = false,
 		opts = {},
-		config = function(_, opts)
-			require("mini.tabline").setup(opts)
-			vim.api.nvim_set_hl(0, "MiniTablineCurrent", { link = "StFilename" })
-			vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { link = "MiniTablineCurrent" })
-			vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", { link = "MiniTablineVisible" })
-			vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", { link = "MiniTablineHidden" })
-		end,
 	},
 	{
 		"stevearc/quicker.nvim",
@@ -46,16 +29,7 @@ return {
 	{
 		"kevinhwang91/nvim-bqf",
 		ft = "qf",
-		opts = {
-			preview = {
-				winblend = 0,
-			},
-		},
-		config = function(_, opts)
-			require("bqf").setup(opts)
-			vim.api.nvim_set_hl(0, "BqfPreviewFloat", { link = "NormalFloat" })
-			vim.api.nvim_set_hl(0, "BqfPreviewTitle", { link = "FloatTitle" })
-		end,
+		opts = { preview = { winblend = 0 } },
 	},
 	{
 		"folke/snacks.nvim",
@@ -85,12 +59,4 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	"NStefan002/screenkey.nvim",
-	-- 	cmd = { "Screenkey" },
-	-- 	opts = {
-	-- 		win_opts = { title = "Input" },
-	-- 	},
-	-- },
-	-- { "norcalli/nvim-colorizer.lua", cmd = { "ColorizerToggle" } },
 }
