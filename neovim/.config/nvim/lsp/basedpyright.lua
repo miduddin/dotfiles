260a3b39920da7651a0ac61cb14ed3761c27c94b
Muhammad Imaduddin <im@dudd.in>
2025-03-29 19:42:54 +0700

    neovim: remove nvim-lspconfig

---
+++ neovim/.config/nvim/lsp/basedpyright.lua
@@ -0,0 +1,21 @@
+return {
+	cmd = { "basedpyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_markers = {
+		"pyproject.toml",
+		"setup.py",
+		"requirements.txt",
+		"Pipfile",
+		"pyrightconfig.json",
+		".git",
+	},
+	settings = {
+		basedpyright = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "openFilesOnly",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+}
