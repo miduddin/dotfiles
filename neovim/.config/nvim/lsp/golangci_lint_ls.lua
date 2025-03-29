2cf8040a9f3ecd3aa62ec74bae60cb7715fc16a5
Muhammad Imaduddin <im@dudd.in>
2025-03-29 19:42:54 +0700

    neovim: remove nvim-lspconfig

---
+++ neovim/.config/nvim/lsp/golangci_lint_ls.lua
@@ -0,0 +1,16 @@
+---@type vim.lsp.Config
+return {
+	cmd = { "golangci-lint-langserver" },
+	filetypes = { "go", "gomod" },
+	init_options = {
+		command = {
+			"golangci-lint",
+			"run",
+			"--output.json.path=stdout",
+			"--show-stats=false",
+			"--issues-exit-code=1",
+		},
+	},
+	root_markers = { "go.mod", ".git" },
+	settings = {},
+}
