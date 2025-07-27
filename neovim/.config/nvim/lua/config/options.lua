7a7093d460ce416ae7779387e4f388ccf12d2a55
Muhammad Imaduddin <im@dudd.in>
2025-07-27 16:43:35 +0700

    neovim: remove unnecessary option changes

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,16 +1,12 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.g.log_level = vim.log.levels.OFF
 vim.g.mapleader = " "
 
 vim.opt.breakindent = true
-vim.opt.conceallevel = 0
 vim.opt.confirm = true
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
 vim.opt.diffopt:append({ "algorithm:patience", "linematch:70" })
-vim.opt.expandtab = false
-vim.opt.fillchars:append({ eob = " ", diff = "╱" })
+vim.opt.fillchars:append({ diff = "╱" })
 vim.opt.foldenable = false
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "indent"
@@ -18,8 +14,7 @@ vim.opt.foldtext = ""
 vim.opt.ignorecase = true
 vim.opt.linebreak = true
 vim.opt.list = true
-vim.opt.listchars:append({ tab = "│  ", trail = "█" })
-vim.opt.mousemodel = "extend"
+vim.opt.listchars:append({ tab = "│  ", trail = "│" })
 vim.opt.number = true
 vim.opt.scrolloff = 5
 vim.opt.shada = ""
