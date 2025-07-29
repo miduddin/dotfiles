72a0021f8f0c8ec4d3ce6b0ac2ea44c8c9906174
Muhammad Imaduddin <im@dudd.in>
2025-07-29 23:42:09 +0700

    neovim(rust): disable lsp semantic tokens

--- neovim/.config/nvim/lsp/rust_analyzer.lua
+++ neovim/.config/nvim/lsp/rust_analyzer.lua
@@ -4,4 +4,5 @@ return {
 	filetypes = { "rust" },
 	root_markers = { "Cargo.toml" },
 	settings = {},
+	on_attach = function(client, _) client.server_capabilities.semanticTokensProvider = nil end,
 }
