1c617ea0b9dd960eaa2ef70c07c275e81e925fe7
Muhammad Imaduddin <im@dudd.in>
2025-08-02 22:52:28 +0700

    cleanup

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,5 @@
 require("nvim-treesitter.configs").setup({
 	auto_install = true,
-	indent = {
-		enable = true,
-		disable = { "yaml" },
-	},
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
