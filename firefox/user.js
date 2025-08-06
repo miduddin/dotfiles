85ca8364add2a4edd0456abc9606286442cb85c1
Muhammad Imaduddin <im@dudd.in>
2025-08-06 15:46:06 +0700

    firefox: cleaner one line toolbook & add some user prefs

--- firefox/user.js
+++ firefox/user.js
@@ -1,4 +1,6 @@
 user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.tabs.groups.smart.enabled", false);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
@@ -6,3 +8,5 @@ user_pref("full-screen-api.warning.delay", 0);
 user_pref("full-screen-api.warning.timeout", 0);
 user_pref("reader.parse-on-load.enabled", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("xpinstall.signatures.required", false);
+user_pref("xpinstall.whitelist.required", false);
