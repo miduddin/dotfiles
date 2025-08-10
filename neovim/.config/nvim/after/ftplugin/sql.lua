710681cb3af502ef7c567cdc39fd52289871f2e8
Muhammad Imaduddin <im@dudd.in>
2025-08-10 12:03:59 +0700

    neovim: move stuff around

--- neovim/.config/nvim/after/ftplugin/sql.lua
+++ neovim/.config/nvim/after/ftplugin/sql.lua
@@ -1,11 +1,17 @@
 vim.opt_local.commentstring = "-- %s"
 
-local psql = require("src.psql")
-vim.api.nvim_buf_create_user_command(0, "Psql", function(opts)
-	psql.query_paragraph(opts.fargs[1], opts.fargs[2])
-end, { nargs = "*" })
+local psql = require("psql")
+vim.api.nvim_buf_create_user_command(
+	0,
+	"Psql",
+	function(opts) psql.query_paragraph(opts.fargs[1], opts.fargs[2]) end,
+	{ nargs = "*" }
+)
 
-local usql = require("src.usql")
-vim.api.nvim_buf_create_user_command(0, "Usql", function(opts)
-	usql.query_paragraph(opts.fargs[1], opts.fargs[2])
-end, { nargs = "*" })
+local usql = require("usql")
+vim.api.nvim_buf_create_user_command(
+	0,
+	"Usql",
+	function(opts) usql.query_paragraph(opts.fargs[1], opts.fargs[2]) end,
+	{ nargs = "*" }
+)
