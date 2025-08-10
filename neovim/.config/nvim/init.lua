9fab06b5c889f38f70fc242ce3d505d0a056fdde
Muhammad Imaduddin <im@dudd.in>
2025-08-10 12:03:59 +0700

    neovim: move stuff around

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,26 +1,3 @@
 vim.cmd("colorscheme vague-custom")
-require("config")
 require("src")
-
-vim.diagnostic.config({
-	jump = { float = true },
-	virtual_text = true,
-	float = { source = true },
-})
-vim.lsp.set_log_level(vim.g.log_level)
-vim.lsp.enable({
-	"cssls",
-	"golangci_lint_ls",
-	"gopls",
-	"html",
-	"jsonls",
-	"lua_ls",
-	"pyright",
-	"ruff",
-	"rust_analyzer",
-	"taplo",
-	"ts_ls",
-	"yamlls",
-})
-
 vim.schedule(function() require("plugins") end)
