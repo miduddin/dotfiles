54e8cc73672b678352620df27ef4bde3cf3c5aa2
Muhammad Imaduddin <im@dudd.in>
2025-10-06 13:48:02 +0700

    neovim: smaller lsp hover height

--- neovim/.config/nvim/lua/keymaps.lua
+++ neovim/.config/nvim/lua/keymaps.lua
@@ -111,6 +111,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		Map("<Leader>xv", toggle_diag_virt_lines, "n", { desc = "Diag: toggle virtual lines", buffer = ev.buf })
 		Map("grd", vim.lsp.buf.definition, "n", { desc = "LSP definition", buffer = ev.buf })
 		Map("grh", toggle_inlay_hint, "n", { desc = "Toggle inlay hint", buffer = ev.buf })
-		Map("K", function() vim.lsp.buf.hover({ max_width = 82 }) end, "n", { desc = "LSP Hover", buffer = ev.buf })
+		Map(
+			"K",
+			function() vim.lsp.buf.hover({ max_width = 82, max_height = 20 }) end,
+			"n",
+			{ desc = "LSP Hover", buffer = ev.buf }
+		)
 	end,
 })
