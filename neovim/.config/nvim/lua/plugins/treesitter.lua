abcc0c99a9a0e06dfc53db114aebd58c41709529
Muhammad Imaduddin <im@dudd.in>
2026-03-21 21:59:56 +0700

    neovim: try showing function header always in ts-context

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -53,4 +53,4 @@ vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = t
 vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
 vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
 
-require("treesitter-context").setup({ max_lines = 2, multiwindow = true })
+require("treesitter-context").setup({ max_lines = 2, multiwindow = true, trim_scope = "inner" })
