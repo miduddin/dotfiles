ff27a5fc4d0cd2280625606363fbc99818f613ae
Muhammad Imaduddin <im@dudd.in>
2026-03-31 13:11:02 +0700

    neovim: update deprecated api

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -1,8 +1,17 @@
+local last_float_bufnr = nil
 vim.diagnostic.config({
-	jump = { float = true },
+	jump = {
+		on_jump = function()
+			if last_float_bufnr and vim.api.nvim_buf_is_valid(last_float_bufnr) then
+				vim.api.nvim_buf_delete(last_float_bufnr, {})
+			end
+			last_float_bufnr, _ = vim.diagnostic.open_float()
+		end,
+	},
 	virtual_text = true,
 	float = { source = true },
 })
+
 vim.lsp.log.set_level(vim.g.log_level)
 vim.lsp.enable({
 	"buf",
