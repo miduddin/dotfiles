21394618c3060b5ef9616236dd8be6a81c645b42
Muhammad Imaduddin <im@dudd.in>
2026-04-07 18:39:37 +0700

    neovim: fix paragraph detection in sql query functions

--- neovim/.config/nvim/lua/utils.lua
+++ neovim/.config/nvim/lua/utils.lua
@@ -36,4 +36,30 @@ function M.write_cmd_output_to_split(obj, bufname)
 	vim.api.nvim_buf_set_lines(buf, 0, -1, true, text)
 end
 
+---@param bufnr integer
+---@param linenr integer 1-based line number.
+local function get_line_text(bufnr, linenr) return vim.api.nvim_buf_get_lines(bufnr, linenr - 1, linenr, false)[1] end
+
+---@return string[]
+function M.get_current_paragraph()
+	local current_line = vim.api.nvim_win_get_cursor(0)[1]
+	assert(vim.trim(get_line_text(0, current_line)) ~= "", "Current line is empty!")
+
+	local line_count = vim.api.nvim_buf_line_count(0)
+
+	local start_line = current_line
+	while start_line > 1 do
+		if vim.trim(get_line_text(0, start_line - 1)) == "" then break end
+		start_line = start_line - 1
+	end
+
+	local end_line = current_line
+	while end_line < line_count do
+		if vim.trim(get_line_text(0, end_line + 1)) == "" then break end
+		end_line = end_line + 1
+	end
+
+	return vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+end
+
 return M
