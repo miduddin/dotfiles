8427be8a43f05d7e330efb74b93bbe28527ea079
Muhammad Imaduddin <im@dudd.in>
2026-06-26 08:28:04 +0700

    neovim: disable new dir plugin, use new autoread plugin

--- neovim/.config/nvim/lua/autocmds.lua
+++ neovim/.config/nvim/lua/autocmds.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-	command = "checktime",
-})
-
 vim.api.nvim_create_autocmd({ "VimResized" }, {
 	command = "wincmd =",
 })
